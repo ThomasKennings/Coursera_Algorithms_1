ceb823030afa251571229ced91027468b8c356ea
Thomas <tomkennings@gmail.com>
2023-06-11 11:47:35 -0400

    Completed Hello World assignment

---
+++ Assignments/Week 1/Hello_World/RandomWord.java
@@ -0,0 +1,28 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String input = "";
+        String output = "";
+        double cycle = 0;
+
+        while (!StdIn.isEmpty()) {
+            cycle += 1;
+
+            input = StdIn.readString();
+
+            if (StdRandom.bernoulli(1 / cycle)) {
+                output = input;
+            }
+        }
+        StdOut.println(output);
+    }
+}
