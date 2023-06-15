b167bedaf253109b98ddabd1c5cd7a04b6b36de7
ThomasKennings <kennings@umich.edu>
2023-06-15 09:45:32 -0400

    Worked on percolation

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Use Git Bash rather than powershell.
 Use "javac -cp .\;algs4.jar javafilename.java" and "java -cp .\;algs4.jar javafilename".
 The "-cp" is the classpath option.
 The ".\;algs4.jar" tells java to look in the current directory (.) and the library (algs4.jar). These are seperated by a semicolon for windows machines (colon for linux). The semicolon has special meaning in Git Bash, so it is escaped with a backslash (\).
+
+To-Do:
+Percolation:
+- Implement error checking (ex: don't allow access out of bounds)
+- Connect site to adjacent sites when opening
+- Implement isFull() (basically done)
+- Implement percolates()
--- Week 1/Percolation/Percolation.java
+++ Week 1/Percolation/Percolation.java
@@ -5,60 +5,90 @@
  **************************************************************************** */
 
 public class Percolation {
-    private int N;
+    private int gridDim;
+    private int imaginaryTopSite;
+    private int imaginaryBottomSite;
+    private int numOpenSites;
     private int[] parentID;
     private int[] size;
     private int[] open;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-        N = n;
+        gridDim = n;
+        imaginaryTopSite = (n * n);
+        imaginaryBottomSite = (n * n) + 1;
+        numOpenSites = 0;
 
-        parentID = new int[n * n];
-        size = new int[n * n];
-        open = new int[n * n];
+        parentID = new int[(n * n) + 2];
+        size = new int[(n * n) + 2];
+        open = new int[(n * n) + 2];
 
-        for (int i = 0; i < (n * n); i++) {
+        for (int i = 0; i < (n * n) + 2; i++) {
             parentID[i] = i;
             size[i] = 1;
+        }
+
+        for (int i = 0; i < gridDim; i++) {
+            union(imaginaryTopSite, i);
+        }
+        for (int i = (gridDim * gridDim) - 1; i > gridDim * (gridDim - 1) - 1; i--) {
+            union(imaginaryBottomSite, i);
         }
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        System.out.println("open(" + row + ", " + col + ")");
+        int site = coords2Site(row, col);
+        open[site] = 1;
+        numOpenSites += 1;
+
         // union this site with adjacent open sites
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return false;
+        System.out.println("isOpen(" + row + ", " + col + ")");
+        return open[coords2Site(row, col)] == 1;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
+        int site = coords2Site(row, col);
+        // return connected(site, imaginary_top_site);
         return false;
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return 0;
+        return numOpenSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
         return false;
     }
-    
+
     public void print() {
-        System.out.println("Site:" + '\t' + "Parent:" + '\t' + "Root:" + '\t' + "Size:");
-        for (int i = 0; i < (N * N); i++) {
+        System.out.println(
+                "Site:" + '\t' + "Parent:" + '\t' + "Root:" + '\t' + "Size:" + '\t' + "Open:");
+        for (int i = 0; i < (gridDim * gridDim) + 2; i++) {
+            if (i == gridDim * gridDim) {
+                System.out.println("--- Imaginary Top Site ---");
+            }
+            if (i == gridDim * gridDim + 1) {
+                System.out.println("--- Imaginary Bottom Site ---");
+            }
             System.out.print(i);
             System.out.print('\t');
             System.out.print(parentID[i]);
             System.out.print('\t');
             System.out.print(root(i));
             System.out.print('\t');
-            System.out.println(size[i]);
+            System.out.print(size[i]);
+            System.out.print('\t');
+            System.out.println(open[i]);
         }
     }
 
@@ -70,10 +100,12 @@ public class Percolation {
     }
 
     private boolean connected(int site1, int site2) {
+        System.out.println("connected(" + site1 + ", " + site2 + ")");
         return root(site1) == root(site2);
     }
 
     private void union(int site1, int site2) {
+        System.out.println("union(" + site1 + ", " + site2 + ")");
         int root1 = root(site1);
         int root2 = root(site2);
 
@@ -81,28 +113,32 @@ public class Percolation {
             return; // Sites are already connected
         }
 
-        if (size[root1] > size[root2]) {
-            parentID[root2] = root1;
-            size[root1] += size[root2];
-        }
-        else {
+        // If size of root1 and root2 are equal, root2 will become a child of root1
+        if (size[root1] < size[root2]) {
             parentID[root1] = root2;
             size[root2] += size[root1];
         }
+        else {
+            parentID[root2] = root1;
+            size[root1] += size[root2];
+        }
     }
 
+    // Takes in [row, col] pair and returns siteID
     private int coords2Site(int row, int col) {
-        return (row * N) + col;
+        return (row * gridDim) + col;
+    }
+
+    // Takes in siteID and returns [row, col] pair
+    private int[] site2Coords(int site) {
+        int[] arr = new int[2];
+        arr[0] = site / gridDim;    // row
+        arr[1] = site % gridDim;    // col
+        return arr;
     }
 
     public static void main(String[] args) {
-        Percolation perc = new Percolation(4);
-        perc.print();
-        System.out.println("Union(0, 1)");
-        perc.union(0, 1);
-        perc.print();
-        System.out.println("Union(1, 3)");
-        perc.union(1, 3);
+        Percolation perc = new Percolation(2);
         perc.print();
     }
 }
