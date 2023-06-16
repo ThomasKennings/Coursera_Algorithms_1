a3cff9136b74c1ff354c42f18e4b9eb3f08fa473
Thomas <tomkennings@gmail.com>
2023-06-15 20:46:57 -0400

    updated percolation variable naming

--- Week 1/Percolation/Percolation.java
+++ Week 1/Percolation/Percolation.java
@@ -5,37 +5,39 @@
  **************************************************************************** */
 
 public class Percolation {
-    private int n;
+    private int matrixWidth;
     private int imaginaryTopSite;
     private int imaginaryBottomSite;
     private int numOpenSites;
     private int[] parentID;
-    private int[] size;
-    private int[] open;
+    private int[] connectedComponentSize;
+    private int[] siteOpenness;
 
-    // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException("Constructor: n must be non-zero and positive.");
+    // creates matrixWidth-by-matrixWidth grid, with all sites initially blocked
+    public Percolation(int matrixWidth) {
+        if (matrixWidth <= 0) {
+            throw new IllegalArgumentException(
+                    "Constructor: matrixWidth must be non-zero and positive.");
         }
-        this.n = n;
-        imaginaryTopSite = (n * n);
-        imaginaryBottomSite = (n * n) + 1;
+        this.matrixWidth = matrixWidth;
+        imaginaryTopSite = (matrixWidth * matrixWidth);
+        imaginaryBottomSite = (matrixWidth * matrixWidth) + 1;
         numOpenSites = 0;
 
-        parentID = new int[(n * n) + 2];
-        size = new int[(n * n) + 2];
-        open = new int[(n * n) + 2];
+        parentID = new int[(matrixWidth * matrixWidth) + 2];
+        connectedComponentSize = new int[(matrixWidth * matrixWidth) + 2];
+        siteOpenness = new int[(matrixWidth * matrixWidth) + 2];
 
-        for (int i = 0; i < (n * n) + 2; i++) {
+        for (int i = 0; i < (matrixWidth * matrixWidth) + 2; i++) {
             parentID[i] = i;
-            size[i] = 1;
+            connectedComponentSize[i] = 1;
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < matrixWidth; i++) {
             union(imaginaryTopSite, i);
         }
-        for (int i = (n * n) - 1; i > n * (n - 1) - 1; i--) {
+        for (int i = (matrixWidth * matrixWidth) - 1; i > matrixWidth * (matrixWidth - 1) - 1;
+             i--) {
             union(imaginaryBottomSite, i);
         }
     }
@@ -47,7 +49,7 @@ public class Percolation {
             throw new IllegalArgumentException("open(): Invalid row or column.");
         }
         int site = coords2Site(row, col);
-        open[site] = 1;
+        siteOpenness[site] = 1;
         numOpenSites += 1;
 
         // connect to adjacent sites if they are open
@@ -75,11 +77,10 @@ public class Percolation {
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        // System.out.println("isOpen(" + row + ", " + col + ")");
         if (invalidRowCol(row, col)) {
             throw new IllegalArgumentException("isOpen(): Invalid row or column.");
         }
-        return open[coords2Site(row, col)] == 1;
+        return siteOpenness[coords2Site(row, col)] == 1;
     }
 
     // is the site (row, col) full?
@@ -98,18 +99,17 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        // System.out.println("Percolates()");
         return connected(imaginaryTopSite, imaginaryBottomSite);
     }
 
     public void print() {
         System.out.println(
                 "Site:" + '\t' + "Parent:" + '\t' + "Root:" + '\t' + "Size:" + '\t' + "Open:");
-        for (int i = 0; i < (n * n) + 2; i++) {
-            if (i == n * n) {
+        for (int i = 0; i < (matrixWidth * matrixWidth) + 2; i++) {
+            if (i == matrixWidth * matrixWidth) {
                 System.out.println("--- Imaginary Top Site ---");
             }
-            if (i == n * n + 1) {
+            if (i == matrixWidth * matrixWidth + 1) {
                 System.out.println("--- Imaginary Bottom Site ---");
             }
             System.out.print(i);
@@ -118,9 +118,9 @@ public class Percolation {
             System.out.print('\t');
             System.out.print(root(i));
             System.out.print('\t');
-            System.out.print(size[i]);
+            System.out.print(connectedComponentSize[i]);
             System.out.print('\t');
-            System.out.println(open[i]);
+            System.out.println(siteOpenness[i]);
         }
     }
 
@@ -132,12 +132,10 @@ public class Percolation {
     }
 
     private boolean connected(int site1, int site2) {
-        // System.out.println("connected(" + site1 + ", " + site2 + ")");
         return root(site1) == root(site2);
     }
 
     private void union(int site1, int site2) {
-        // System.out.println("union(" + site1 + ", " + site2 + ")");
         int root1 = root(site1);
         int root2 = root(site2);
 
@@ -145,32 +143,32 @@ public class Percolation {
             return; // Sites are already connected
         }
 
-        // If size of root1 and root2 are equal, root2 will become a child of root1
-        if (size[root1] < size[root2]) {
+        // If connectedComponentSize of root1 and root2 are equal, root2 will become a child of root1
+        if (connectedComponentSize[root1] < connectedComponentSize[root2]) {
             parentID[root1] = root2;
-            size[root2] += size[root1];
+            connectedComponentSize[root2] += connectedComponentSize[root1];
         }
         else {
             parentID[root2] = root1;
-            size[root1] += size[root2];
+            connectedComponentSize[root1] += connectedComponentSize[root2];
         }
     }
 
     // Takes in [row, col] pair and returns siteID
     private int coords2Site(int row, int col) {
-        return (row * n) + col;
+        return (row * matrixWidth) + col;
     }
 
     // Takes in siteID and returns [row, col] pair
     private int[] site2Coords(int site) {
         int[] arr = new int[2];
-        arr[0] = site / n;    // row
-        arr[1] = site % n;    // col
+        arr[0] = site / matrixWidth;    // row
+        arr[1] = site % matrixWidth;    // col
         return arr;
     }
 
     private boolean invalidRowCol(int row, int col) {
-        return ((row < 0) || (row >= n) || (col < 0) || (col >= n));
+        return ((row < 0) || (row >= matrixWidth) || (col < 0) || (col >= matrixWidth));
     }
 
     public static void main(String[] args) {
